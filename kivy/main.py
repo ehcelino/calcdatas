1174c896043a8787da8bd953a021d89db7cd5097
Eduardo C <ehcelino@gmail.com>
2022-08-31 12:47:35 -0300

    Organização para o github

--- kivy/main.py
+++ kivy/main.py
@@ -10,8 +10,11 @@ from kivy.uix.textinput import TextInput
 import re
 import numpy as np
 import locale
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
 
 Window.size = (300, 550)
+Window.clearcolor = (1, 1, 1, 1)
 
 regexdata = re.compile(r'^(\d{2})[-.\/](\d{2})[-.\/](\d{4})$')
 regexvalor = re.compile(r'(\d)*')
@@ -33,6 +36,13 @@ class CDataApp(App):
         pass
         # return Button(text='Hello World')
 
+    def open_help(self):
+        popup = Popup(title='Test popup',
+                      content=Label(text='Hello world'),
+                      size_hint=(None, None), size=(400, 400))
+        popup.open()
+
+
     def valida_data(self, data):
         resultado =  False
         try:
@@ -41,6 +51,52 @@ class CDataApp(App):
         except ValueError:
             resultado = False
         return resultado
+
+    def strip_datas(self, datas):
+        result = True
+        datas = datas.replace(" ", "")
+        if datas == '':
+            result = False
+            return 'ERRO'
+        elif datas.find('-') == -1:
+            result = False
+            return 'ERRO'
+        else:
+            strings = datas.split('-')
+            if not re.fullmatch(regexdata, strings[0]) or not re.fullmatch(regexdata, strings[1]):
+                result = False
+                return 'ERRO'
+            else:
+                return strings
+
+    def valida(self, data, valor):
+        tmp = True
+        if not re.fullmatch(regexdata, data):
+            tmp = False
+        try:
+            tmp = bool(datetime.strptime(data, '%d/%m/%Y'))
+        except ValueError:
+            tmp = False
+        if not re.fullmatch(regexvalor, valor):
+            tmp = False
+        return tmp
+
+    def strip_operacao(self, operacao=''):
+        op_limpo = operacao.replace(" ", "")
+        if op_limpo.find('+') != -1:
+            strings = op_limpo.split('+')
+            if not self.valida(strings[0], strings[1]):
+                return 'ERRO'
+            else:
+                return strings[0], int(strings[1]), '+'
+        elif op_limpo.find('-') != -1:
+            strings = op_limpo.split('-')
+            if not self.valida(strings[0], strings[1]):
+                return 'ERRO'
+            else:
+                return strings[0], int(strings[1]), '-'
+        else:
+            return 'ERRO'
 
     def ler_operacao(self):
         resultado = None
@@ -104,96 +160,99 @@ class CDataApp(App):
             return data_final_str
 
     def botao_dias(self):
-        tmp = self.ler_operacao()
+        tmp = self.strip_operacao(self.root.ids.ti_operacao.text)
         if tmp == 'ERRO':
             print(tmp)
             print('Operação inválida.')
-        elif tmp[2] not in ('+', '-'):
-            print('Operação inválida.')
+            self.root.ids.ti_resultado.text = 'Operação inválida.'
         else:
             result = self.calcula(tmp[0], tmp[1], tmp[2], 'D')
             self.root.ids.ti_resultado.text = result
+            historico = self.root.ids.ti_historico.text
+            self.root.ids.ti_historico.text = \
+                historico + '\n' + tmp[0] + ' ' + tmp[2] + ' ' + str(tmp[1]) + ' dias' + '\n= ' + result
 
     def botao_meses(self):
-        tmp = self.ler_operacao()
+        tmp = self.strip_operacao(self.root.ids.ti_operacao.text)
         if tmp == 'ERRO':
+            print(tmp)
             print('Operação inválida.')
-        elif tmp[2] not in ('+', '-'):
-            print('Operação inválida.')
+            self.root.ids.ti_resultado.text = 'Operação inválida.'
         else:
-            result = self.calcula(tmp[0], tmp[1], tmp[2], 'M')
+            result = self.calcula(tmp[0], tmp[1], tmp[2], 'D')
             self.root.ids.ti_resultado.text = result
+            historico = self.root.ids.ti_historico.text
+            self.root.ids.ti_historico.text = \
+                historico + '\n' + tmp[0] + ' ' + tmp[2] + ' ' + str(tmp[1]) + ' meses' + '\n= ' + result
+
 
     def botao_anos(self):
-        tmp = self.ler_operacao()
+        tmp = self.strip_operacao(self.root.ids.ti_operacao.text)
         if tmp == 'ERRO':
+            print(tmp)
             print('Operação inválida.')
-        elif tmp[2] not in ('+', '-'):
-            print('Operação inválida.')
+            self.root.ids.ti_resultado.text = 'Operação inválida.'
         else:
-            result = self.calcula(tmp[0], tmp[1], tmp[2], 'A')
+            result = self.calcula(tmp[0], tmp[1], tmp[2], 'D')
             self.root.ids.ti_resultado.text = result
-        # tmp = self.ler_operacao()
-        # data_final = None
-        # print('tmp: ', tmp)
-        # if tmp == 'ERRO':
-        #     print('Operação inválida')
-        # try:
-        #     if tmp[2] == '+':
-        #         datatmp = datetime.strptime(tmp[0], '%d/%m/%Y')
-        #         data_final = datatmp + relativedelta(days=int(tmp[1]))
-        #         print('positivo')
-        #         self.root.ids.ti_resultado.text = datetime.strftime(data_final, '%d/%m/%Y')
-        #     if tmp[2] == '-':
-        #         datatmp = datetime.strptime(tmp[0], '%d/%m/%Y')
-        #         data_final = datatmp - relativedelta(days=int(tmp[1]))
-        #         print('negativo')
-        #         self.root.ids.ti_resultado.text = datetime.strftime(data_final, '%d/%m/%Y')
-        # except Exception as err:
-        #     print('Operação inválida: ', err)
-        #     self.root.ids.ti_resultado.text = 'ERRO'
-        #     pass
+            historico = self.root.ids.ti_historico.text
+            self.root.ids.ti_historico.text = \
+                historico + '\n' + tmp[0] + ' ' + tmp[2] + ' ' + str(tmp[1]) + ' anos' + '\n= ' + result
+
 
     def botao_hoje(self):
         self.root.ids.ti_operacao.text = datetime.strftime(date.today(), '%d/%m/%Y')
 
     def botao_dias_uteis(self):
-        tmp = self.ler_operacao()
+        # tmp = self.ler_operacao()
+        tmp = self.strip_datas(self.root.ids.ti_operacao.text)
         print('TMP: ', tmp)
         if tmp == 'ERRO':
             print('As datas devem ser inseridas da seguinte forma: 00/00/0000-00/00/0000')
-        elif tmp[2] not in ('+', '-'):
-            print('Operação inválida.')
+            self.root.ids.ti_resultado.text = 'Operação inválida.'
+        # elif tmp[2] not in ('+', '-'):
+        #     print('Operação inválida.')
         else:
             print('TMP 0 and 1', tmp[0], tmp[1])
             data1 = datetime.strptime(tmp[0], '%d/%m/%Y')
             data2 = datetime.strptime(tmp[1], '%d/%m/%Y')
             tmp2 = np.busday_count(data1.strftime('%Y-%m-%d'), data2.strftime('%Y-%m-%d'))
             self.root.ids.ti_resultado.text = str(abs(tmp2)) + ' dias'
-
+            historico = self.root.ids.ti_historico.text
+            self.root.ids.ti_historico.text = \
+                historico + '\nEntre ' + tmp[0] + ' e ' + tmp[1] + ' há\n' + str(abs(tmp2)) + ' dias úteis.'
 
     def botao_dia_semana(self):
         tmp = self.ler_operacao()
         print('TMP: ', tmp)
         if tmp == 'ERRO':
             print('As datas devem ser inseridas da seguinte forma: 00/00/0000-00/00/0000')
+            self.root.ids.ti_resultado.text = 'Operação inválida.'
         else:
             dt_tmp = datetime.strptime(tmp, '%d/%m/%Y')
-            tmp = datetime.strftime(dt_tmp, '%A')
-            self.root.ids.ti_resultado.text = tmp
+            tmp2 = datetime.strftime(dt_tmp, '%A')
+            self.root.ids.ti_resultado.text = tmp2
+            historico = self.root.ids.ti_historico.text
+            self.root.ids.ti_historico.text = \
+                historico + '\n' + 'Dia ' + tmp + ' = ' + tmp2
 
     def botao_diferenca(self):
-        tmp = self.ler_operacao()
+        # tmp = self.ler_operacao()
+        tmp = self.strip_datas(self.root.ids.ti_operacao.text)
         print('TMP: ', tmp)
         if tmp == 'ERRO':
             print('As datas devem ser inseridas da seguinte forma: 00/00/0000-00/00/0000')
-        elif tmp[2] not in ('+', '-'):
-            print('Operação inválida.')
+            self.root.ids.ti_resultado.text = 'Operação inválida.'
+        # elif tmp[2] not in ('+', '-'):
+        #     print('Operação inválida.')
         else:
             data1 = datetime.strptime(tmp[0], '%d/%m/%Y')
             data2 = datetime.strptime(tmp[1], '%d/%m/%Y')
             timedelta = data1 - data2
             self.root.ids.ti_resultado.text = str(abs(timedelta.days)) + ' dias'
+            historico = self.root.ids.ti_historico.text
+            self.root.ids.ti_historico.text = \
+                historico + '\n' + tmp[0] + ' - ' + tmp[1] + ' = ' + str(abs(timedelta.days)) + ' dias'
 
     def close(self):
         self.stop()
